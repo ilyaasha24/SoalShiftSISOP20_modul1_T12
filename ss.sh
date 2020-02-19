fa11653596f5c7e107aca2e172f60afa46bb8708
IzuruSakamaki <50161133+IzuruSakamaki@users.noreply.github.com>
2020-02-20 00:26:33 +0700

    Add files via upload

--- ss.sh
+++ ss.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
-a=($(md5sum kucing* | awk -F "  " '{a[$1]=$2}END{for(i in a)print i, a[i]}' | uniq |  awk -F " " '{print $2}' | sort))
+#a=($(md5sum kucing* | awk -F "  " '{a[$1]=$2}END{for(i in a)print i, a[i]}' | uniq | awk -F " " '{print $2}' | sort))
 b=($(ls kucing* | sort))
+grep 'Location:' wget.log | cut -c32-53 | tr -d "_" > location.log
+a=($(paste "./location.log" <(echo -e "kucing_"{1..28}",\n") | tr -d " " | sed 's/\t/,/g' | awk -F "," '{a[$1]=$2}END{for(i in a)print i, a[i]}' | sort -k2 | awk -F " " 'FNR>1{print $2}'))
 #echo ${a[@]}
 #echo ${b[@]}
-Array3=()
+c=()
 for i in "${b[@]}"; do
     skip=
     for j in "${a[@]}"; do
         [[ $i == $j ]] && { skip=1; break; }
     done
-    [[ -n $skip ]] || Array3+=("$i")
+    [[ -n $skip ]] || c+=("$i")
 done
-echo ${Array3[*]}
+#echo ${c[@]}
+d=$(ls -1 "./duplicate/" | wc -l)
+for i in "${c[@]}"; do
+    let d+=1
+    mv $i "./duplicate/duplicate_$d"
+done
+k=$(ls -1 "./kenangan" | wc -l)
+for i in "${a[@]}"; do
+    let k+=1
+    mv $i "./kenangan/kenangan_$k"
+done
+mv ./wget.log{,.bak}
