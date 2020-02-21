20b1525a5c4c8f27dacaca03ef53aca0df463fc1
Muhammad Ilya Asha Soegondo <55181949+ilyaasha24@users.noreply.github.com>
2020-02-21 18:08:15 +0700

    Update soal2.sh

--- soal2/soal2.sh
+++ soal2/soal2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-file=$(echo $1 | tr -dc 'a-zA-Z')
+file=$(echo $1 )
+file=$(echo ${file%%.*} | tr -dc 'a-zA-Z')
 rand28=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 28 | head -n 1)
 echo "$rand28" > "$file.txt"
