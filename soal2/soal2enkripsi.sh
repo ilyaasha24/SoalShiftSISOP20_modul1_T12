c67cc5b1c3d80cfe8f4320084b53320c545f6e62
Muhammad Ilya Asha Soegondo <55181949+ilyaasha24@users.noreply.github.com>
2020-02-21 18:00:34 +0700

    Update soal2enkripsi.sh

--- soal2/soal2enkripsi.sh
+++ soal2/soal2enkripsi.sh
@@ -12,7 +12,8 @@ chr() {
   printf \\$(printf '%03o' $1)
 }
 #main
-file=$(echo $1 | tr -dc 'a-zA-Z')
+file=$(echo $1 )
+file=$(echo ${file%%.*} | tr -dc 'a-zA-Z')
 crtime=($(genesis "$file.txt"))
 crhour=$(echo ${crtime[4]} | head -c2)
 shift=$(chr "$(($crhour + 65))")-ZA-$(chr "$(($crhour + 64))")$(chr "$(($crhour + 97))")-za-$(chr "$(($crhour + 96))")
